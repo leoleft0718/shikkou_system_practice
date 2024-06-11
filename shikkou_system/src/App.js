4bc249aa627bb3aedb7715494acd090a0ce0369f
leoleftpc2 <xiangdasanpu@gmail.com>
2024-06-11 14:14:21 +0900

    react-router-dom追加確認済み

---
+++ shikkou_system/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Home from "./Home";
+import About from "./About";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<h1>Not Found Page</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
